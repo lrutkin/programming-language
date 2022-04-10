f9ffa79ff067a59a7e7ee42fa303e1b34514b80d
Leslie Rutkin <lrutkin@gmail.com>
2022-04-09 18:11:12 -0700

    add initial js functionality

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#selector").submit(function(event) {
+    event.preventDefault();
+
+    const qOneInput=parseInt($("#q1").val());
+    const qTwoInput=parseInt($("#q2").val());
+    const qThreeInput=parseInt($("#q3").val());
+    const qFourInput=parseInt($("#q4").val());
+    const qFiveInput=parseInt($("#q5").val());
+
+    let final=qOneInput+qTwoInput+qThreeInput+qFourInput+qFiveInput
+    if (final <=5) {
+      $("#resultRuby").show();
+    } else if (final <=10){
+      $("#resultJavaScript").show();
+    } else 
+      $("#resultC").show();
+  });
+});
